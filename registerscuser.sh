2d8c614b284d633b101a7c389344b5307c4b1014
Richard Purves <richardpurves@MacBook-Pro.local>
2017-08-04 10:32:03 +0200

    V0.2
    Reworked ahead of JNUC 2017

--- registerscuser.sh
+++ registerscuser.sh
@@ -1,33 +1,10 @@
 #!/bin/bash
 
 # Script to grab smartcard hash and program it to the current user
-# Horrible hack of sc_auth script provided on OS X
+# Needs root. Assumes first hash is the smart card. Bad bad bad, needs error checking.
 
 loggedInUser=$( python -c 'from SystemConfiguration import SCDynamicStoreCopyConsoleUser; import sys; username = (SCDynamicStoreCopyConsoleUser(None, None, None) or [None])[0]; username = [username,""][username in [u"loginwindow", None, u""]]; sys.stdout.write(username + "\n");' )
+hash=$( sc_auth hash | awk 'NR==1{ print $1 }' )
 
-hash=$( /usr/bin/security dump-keychain |
-awk -v RE="$loggedInUser" '
-/^    0x00000001/	{
-	if (matched = ($2 ~ RE)) { name=$0; sub("^.*<blob>=\"", "", name); sub("\"$", "", name); count++; }}
-/^    0x00000006/	{
-	if (matched) { hash=$2; sub("<blob>=0x", "", hash); print hash; }}
-'
-
-/usr/bin/security dump-keychain |
-awk -v RE="$loggedInUser" '
-/^    0x01000000/	{
-	if (matched = ($2 ~ RE)) { name=$0; sub("^.*<blob>=\"", "", name); sub("\"$", "", name); count++; }}
-/^    0x06000000/	{
-	if (matched) { hash=$2; sub("<blob>=0x", "", hash); print hash; }}
-')
-
-if [ "$hash" = "" ];
-then
-	echo "Hash not found."
-	exit 1
-else
-	echo "Hash $hash found. Assigning to user $loggedInUser"
-	dscl . -append "/Users/$loggedInUser" AuthenticationAuthority ";pubkeyhash;$hash"
-fi
-
-exit 0
+echo "Hash $hash found. Assigning to user $loggedInUser"
+sc_auth accept -u $loggedInUser -h $hash

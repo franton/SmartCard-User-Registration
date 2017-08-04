8824a3dc4a4db5ef01fa507fa1e1e216f7a7fcc3
Richard Purves <richardpurves@MacBook-Pro.zyxel.com>
2017-08-04 19:45:03 +0200

    CTK version

--- registerscuser-ctk.sh
+++ registerscuser-ctk.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-# Script to grab smartcard hash and program it to the current user
+# Script to grab CTK smartcard hash and program it to the current user
 # Needs root. Assumes first hash is the smart card. Bad bad bad, needs error checking.
 
 loggedInUser=$( python -c 'from SystemConfiguration import SCDynamicStoreCopyConsoleUser; import sys; username = (SCDynamicStoreCopyConsoleUser(None, None, None) or [None])[0]; username = [username,""][username in [u"loginwindow", None, u""]]; sys.stdout.write(username + "\n");' )
 
-hash=$( sc_auth hash | awk 'NR==1{ print $1 }' )
+hash=$( sc_auth list | awk 'NR==1{ print $2 }' )
 
 echo "Hash $hash found. Assigning to user $loggedInUser"
-sc_auth accept -u $loggedInUser -h $hash
+sc_auth pair -u $loggedInUser -h $hash
